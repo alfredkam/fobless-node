9b3488885d5d8c460dba2dd96f3ed9d3dfccf7fb
alfredkam <akam@adcnx.com>
2015-04-21 22:21:23 -0700

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+var twilioSID = process.env.TWILIO_ACCOUNT_SID || '';
+var numbers = (process.env.AUTHORIZED_NUMBERS || '').split(",");
+var validNumbers = {};
+for (var i in numbers) {
+    validNumbers[numbers[i]] = true;
+}
+
+app.get('/twilio/voice', function (req, res) {
+    var query = req.query;
+    if (query.AccountSid == twilioSID && validNumbers[query.from]) {
+        res.send('<?xml version="1.0" encoding="UTF-8"?>' +
+                 '<Response>' +
+                     '<Play digits="www9"/>' +
+                     '</Response>');
+    }
+});
+
+var server = app.listen(port, function () {
+    console.log('Listening on port ' + server.address().port);
+});
